308bc631d6265e60cc353e58067f512e460aca95
Thanakorn Limpanawuthi <thnk2809@gmail.com>
2022-09-16 20:05:59 +0700

    Saved

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,28 @@
+package nl.tudelft.jpacman.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+public class BoardTest {
+    @Test void testInvariant(){
+        Square row = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = row;
+        Board board = new Board(grid);
+        assertThat(board.invariant()).isTrue();
+    }
+    @Test void testSquareAt(){
+        Square row = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = row;
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0,0)).isEqualTo(row);
+    }
+    @Test void testSquareAtNullSquare(){
+        Square row = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+//        grid[0][0] = row;
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0,0)).isEqualTo(row);
+    }
+}
