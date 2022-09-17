fda4d21e65ca4961fcda0cf6eced6b8281626e39
Thanakorn Limpanawuthi <thanakorn.limpanawuthi@agoda.com>
2022-09-17 15:42:25 +0700

    Submit

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -11,18 +11,10 @@ public class BoardTest {
         Board board = new Board(grid);
         assertThat(board.invariant()).isTrue();
     }
-    @Test void testSquareAt(){
-        Square row = new BasicSquare();
+    @Test
+    void testSquareAt(){
         Square[][] grid = new Square[1][1];
-        grid[0][0] = row;
         Board board = new Board(grid);
-        assertThat(board.squareAt(0,0)).isEqualTo(row);
-    }
-    @Test void testSquareAtNullSquare(){
-        Square row = new BasicSquare();
-        Square[][] grid = new Square[1][1];
-//        grid[0][0] = row;
-        Board board = new Board(grid);
-        assertThat(board.squareAt(0,0)).isEqualTo(row);
+        assertThat(board.squareAt(0,0)).isEqualTo(null);
     }
 }
