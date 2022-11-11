0399f388c859dba5a754ba997c31bd3918e30e3d
Thanakorn Limpanawuthi <thanakorn.limpanawuthi@agoda.com>
2022-11-11 22:22:19 +0700

    Fix error

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,6 +1,6 @@
 package nl.tudelft.jpacman.board;
 
-import static org.assert.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 public class BoardTest {
