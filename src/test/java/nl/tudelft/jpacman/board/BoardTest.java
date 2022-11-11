2c3258274194e01862f57d3a758af9ef180af660
Thanakorn Limpanawuthi <thanakorn.limpanawuthi@agoda.com>
2022-11-11 22:24:28 +0700

    Fix error

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,7 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 public class BoardTest {
-    @Test void testInvariant(){
+    @Test 
+    void testInvariant(){
         Square row = new BasicSquare();
         Square[][] grid = new Square[1][1];
         grid[0][0] = row;
